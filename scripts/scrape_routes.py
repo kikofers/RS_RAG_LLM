3a4213a9a9e540adfaff371a4f5e120b5bb034df
kikofers <you@example.com>
2025-05-24 15:05:40 +0300

    Working route scraper.

--- scripts/scrape_routes.py
+++ scripts/scrape_routes.py
@@ -4,6 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import json
 import time
+import os
 
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
@@ -23,26 +24,26 @@ route_count = len(route_links)
 for i in range(route_count):
     route_links = driver.find_elements(By.CSS_SELECTOR, "#schedule > div.container-fluid > div:nth-child(3) > ul > li > a")
     route_el = route_links[i]
-    route_name = route_el.text.strip()
+    route_text = route_el.text.strip()
+    route_number = ''.join(filter(str.isdigit, route_text))
     route_el.click()
 
     wait.until(EC.presence_of_all_elements_located(
         (By.CSS_SELECTOR, "#schedule4 > div.container-fluid > div.row-fluid.row-directions > ul > li > a")
     ))
-    time.sleep(0.5)
+    time.sleep(0.2)
 
     directions = []
     direction_elements = driver.find_elements(By.CSS_SELECTOR, "#schedule4 > div.container-fluid > div.row-fluid.row-directions > ul > li > a")
     for j in range(len(direction_elements)):
-        # re-fetch to avoid stale reference
         dir_el = driver.find_elements(By.CSS_SELECTOR, "#schedule4 > div.container-fluid > div.row-fluid.row-directions > ul > li > a")[j]
         directions.append({
-            "name": dir_el.text.strip(),
+            "name": dir_el.get_attribute("textContent").strip(),
             "url": dir_el.get_attribute("href")
         })
 
     routes.append({
-        "route_name": route_name,
+        "route_name": route_number,
         "directions": directions
     })
 
@@ -50,9 +51,10 @@ for i in range(route_count):
     wait.until(EC.presence_of_all_elements_located(
         (By.CSS_SELECTOR, "#schedule > div.container-fluid > div:nth-child(3) > ul > li > a")
     ))
-    time.sleep(0.5)
+    time.sleep(0.2)
 
 driver.quit()
 
-with open("data/routes_with_directions.json", "w", encoding="utf-8") as f:
+os.makedirs("data", exist_ok=True)
+with open("data/routes.json", "w", encoding="utf-8") as f:
     json.dump(routes, f, ensure_ascii=False, indent=2)
