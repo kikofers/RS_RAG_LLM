a51793c56d6c45582e0b76a45d18d26d2270aefb
kikofers <you@example.com>
2025-05-24 12:35:40 +0300

    First working route scraping script. Needs adjustments.

--- scripts/scrape_routes.py
+++ scripts/scrape_routes.py
@@ -2,65 +2,57 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 import json
+import time
 
 options = webdriver.ChromeOptions()
 options.add_argument("--start-maximized")
 driver = webdriver.Chrome(options=options)
 
-base_url = "https://saraksti.rigassatiksme.lv/index.html"
-driver.get(base_url)
+driver.get("https://saraksti.lv/#lv")
 
-WebDriverWait(driver, 30).until(
-    EC.presence_of_all_elements_located((By.CSS_SELECTOR, "table#tblRoutes a"))
-)
+wait = WebDriverWait(driver, 30)
+wait.until(EC.presence_of_all_elements_located(
+    (By.CSS_SELECTOR, "#schedule > div.container-fluid > div:nth-child(3) > ul > li > a")
+))
 
-routes_elements = driver.find_elements(By.CSS_SELECTOR, "table#tblRoutes td:nth-child(2) a")
 routes = []
+route_links = driver.find_elements(By.CSS_SELECTOR, "#schedule > div.container-fluid > div:nth-child(3) > ul > li > a")
+route_count = len(route_links)
 
-for route_el in routes_elements:
+for i in range(route_count):
+    route_links = driver.find_elements(By.CSS_SELECTOR, "#schedule > div.container-fluid > div:nth-child(3) > ul > li > a")
+    route_el = route_links[i]
     route_name = route_el.text.strip()
-    href = route_el.get_attribute("href")
-    if not href:
-        continue
-
     route_el.click()
-    WebDriverWait(driver, 30).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "#spanDir1"))
-    )
 
-    time.sleep(1)
-    dir1_span = driver.find_element(By.CSS_SELECTOR, "#spanDir1")
-    dir1_text = dir1_span.text.strip()
-    dir1_href = driver.current_url
-    dir1_span.click()
-    time.sleep(1)
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_all_elements_located((By.CSS_SELECTOR, "#ulDirections a"))
-    )
+    wait.until(EC.presence_of_all_elements_located(
+        (By.CSS_SELECTOR, "#schedule4 > div.container-fluid > div.row-fluid.row-directions > ul > li > a")
+    ))
+    time.sleep(0.5)
 
-    directions = driver.find_elements(By.CSS_SELECTOR, "#ulDirections a")
-    direction_data = []
-    for dir_el in directions:
-        direction_data.append({
+    directions = []
+    direction_elements = driver.find_elements(By.CSS_SELECTOR, "#schedule4 > div.container-fluid > div.row-fluid.row-directions > ul > li > a")
+    for j in range(len(direction_elements)):
+        # re-fetch to avoid stale reference
+        dir_el = driver.find_elements(By.CSS_SELECTOR, "#schedule4 > div.container-fluid > div.row-fluid.row-directions > ul > li > a")[j]
+        directions.append({
             "name": dir_el.text.strip(),
             "url": dir_el.get_attribute("href")
         })
 
     routes.append({
         "route_name": route_name,
-        "main_url": dir1_href,
-        "directions": direction_data
+        "directions": directions
     })
 
     driver.back()
-    WebDriverWait(driver, 30).until(
-        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "table#tblRoutes a"))
-    )
-    routes_elements = driver.find_elements(By.CSS_SELECTOR, "table#tblRoutes td:nth-child(2) a")
+    wait.until(EC.presence_of_all_elements_located(
+        (By.CSS_SELECTOR, "#schedule > div.container-fluid > div:nth-child(3) > ul > li > a")
+    ))
+    time.sleep(0.5)
 
 driver.quit()
 
-with open("routes_with_directions.json", "w", encoding="utf-8") as f:
+with open("data/routes_with_directions.json", "w", encoding="utf-8") as f:
     json.dump(routes, f, ensure_ascii=False, indent=2)
